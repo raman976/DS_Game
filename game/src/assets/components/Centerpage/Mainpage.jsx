d2b3330392b2aec5a7107c6f1bc5a72715210b2d
Raman Pandey <ramanpandey413@gmail.com>
2025-03-26 17:05:33 +0530

    Logic Completed

--- game/src/assets/components/Centerpage/Mainpage.jsx
+++ game/src/assets/components/Centerpage/Mainpage.jsx
@@ -1,35 +1,60 @@
-import React from 'react'
-import { StyledCards, StyledGrid } from './Grid.Styled'
-import { HashiraImages } from '../Constants'
-import { DemonImages } from '../Constants'
+import React from "react";
+import { useState } from "react";
+import { StyledCards, StyledGrid } from "./Grid.Styled";
+import { HashiraImages, PlaceHolder } from "../Constants";
+import { DemonImages } from "../Constants";
 const Mainpage = () => {
-
-  const CopyHashiraImages=[...HashiraImages]
-    function shuffleArray(array) {
-      
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1)); 
-          [array[i], array[j]] = [array[j], array[i]]; 
-        }
-        return array;
-      }
-
-
-      
-      let NewArray=shuffleArray([...DemonImages,...HashiraImages,...CopyHashiraImages])
-      console.log(NewArray)
-      
-  return (
-   <StyledGrid>
-
-    {NewArray.map((i,index)=>{
-        return (<StyledCards key={index} >
-          <img src={i}></img>
-        </StyledCards>)
-    })
+  const CopyHashiraImages = [...HashiraImages];
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
-   </StyledGrid>
-  )
-}
+    return array;
+  }
+  const [NewArray, setNewArray] = useState(() =>
+    shuffleArray([...DemonImages, ...HashiraImages, ...CopyHashiraImages])
+  );
 
-export default Mainpage
+  console.log(NewArray);
+
+  const [SelectedImages, SetSelectedImages] = useState([]);
+  const [MatchedImages, SetMatchedImages] = useState(new Set());
+  function HandeleClick(index) {
+    if (SelectedImages.length == 2 || MatchedImages.has(index)) return;
+    const NewSelectedCards = [...SelectedImages, index];
+    SetSelectedImages(NewSelectedCards);
+    if (NewSelectedCards.length == 2) {
+      if (NewArray[NewSelectedCards[0]] === NewArray[NewSelectedCards[1]]) {
+        SetMatchedImages(
+          (prev) => new Set([...prev, NewSelectedCards[0], NewSelectedCards[1]])
+        );
+        SetSelectedImages([]);
+      } else {
+        setTimeout(() => {
+          SetSelectedImages([]);
+        }, 1000);
+      }
+    }
+  }
+
+  return (
+    <StyledGrid>
+      {NewArray.map((i, index) => {
+        return (
+          <StyledCards key={index} onClick={() => HandeleClick(index)}>
+            <img
+              src={
+                MatchedImages.has(index) || SelectedImages.includes(index)
+                  ? i
+                  : PlaceHolder
+              }
+            ></img>
+          </StyledCards>
+        );
+      })}
+    </StyledGrid>
+  );
+};
+
+export default Mainpage;
