f2232648605c3b7aa884c5cce4f6c12bc5177437
Raman Pandey <ramanpandey413@gmail.com>
2025-03-27 11:36:22 +0530

    updated site icon

---
+++ game/Constants.jsx
@@ -0,0 +1,18 @@
+export const HashiraImages = [
+  "https://www.demonslayer-api.com/api/v1/characters/images/29.webp",
+  "https://www.demonslayer-api.com/api/v1/characters/images/1.webp",
+  "https://www.demonslayer-api.com/api/v1/characters/images/2.webp",
+  "https://www.demonslayer-api.com/api/v1/characters/images/12.webp",
+
+  "https://www.demonslayer-api.com/api/v1/characters/images/20.webp",
+  "https://www.demonslayer-api.com/api/v1/characters/images/28.webp",
+  "https://www.demonslayer-api.com/api/v1/characters/images/9.webp",
+];
+
+export const DemonImages = [
+  "https://www.demonslayer-api.com/api/v1/characters/images/17.webp",
+  "https://www.demonslayer-api.com/api/v1/characters/images/14.webp",
+];
+
+export const PlaceHolder =
+  "https://images.seeklogo.com/logo-png/40/1/demon-slayer-logo-png_seeklogo-408228.png";
